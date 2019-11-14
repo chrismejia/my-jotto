5b0faf81a494297464a5282d2115b91764af0409
Christian Mejia <chrismejia10@gmail.com>
2019-11-13 20:01:33 -0500

    Add base code and test imports for GuessedWords

---
+++ src/GuessedWords.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GuessedWords = () => {
+  return <div />;
+};
+
+GuessedWords.propTypes = {
+  //
+};
+
+export default GuessedWords;
