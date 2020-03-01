35a6fd165cc0a794fe188fb8be4c28a664772b4b
Christian Mejia <chrismejia10@gmail.com>
2020-02-29 22:01:33 -0500

    Add actionTypes and correctGuess function; write correctGuess() test

---
+++ src/actions/index.test.js
@@ -0,0 +1,11 @@
+import { correctGuess, actionTypes } from "./";
+
+describe("correctGuess", () => {
+  test("returns an action with type `CORRECT_GUESS`", () => {
+    const action = correctGuess();
+
+    // Because we're dealing with mutable objects, we can't use toBe here since one {} !== a different {}. We use toEqual since it's a deep equal.
+
+    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+  });
+});
