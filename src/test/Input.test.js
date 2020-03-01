b2f751d88be21ab79b9aea3c057ad3b0524c6977
Christian Mejia <chrismejia10@gmail.com>
2020-03-01 03:19:28 -0500

    Reorganize all test files into the test folder

--- src/test/Input.test.js
+++ src/test/Input.test.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { findByTestAttr } from "./test/testUtils";
-import Input from "./Input";
+import { findByTestAttr } from "./testUtils";
+import Input from "../Input";
+
+// This old setup doesn't work because we don't have a store set up.
+// It throws an Invariant Violation talking about how it can't find a store.
+//
+// const setup = (initialState = {}) => {
+//   const wrapper = shallow(<Input />);
+//   console.log(wrapper.debug());
+// };
+//
+// setup();
+//
+// In the testUtils, we'll make a storeFactory, for fresh stores for each test suite
 
 describe("render", () => {
   describe("while the word has not been guessed", () => {
--- src/test/successReducer.test.js
+++ src/test/successReducer.test.js
@@ -1,5 +1,5 @@
 import { actionTypes } from "../actions";
-import successReducer from "./successReducer";
+import successReducer from "../reducers/successReducer";
 
 // We want to test to ensure that if no action and no state is passed in, we return default (false).
 // Additional we want to return true if the CORRECT_GUESS action type is passed in.
