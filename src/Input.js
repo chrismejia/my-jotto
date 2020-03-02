0031f134da8e206ff0b27902ecc808eb38a1b44e
Christian Mejia <chrismejia10@gmail.com>
2020-03-02 03:52:42 -0500

    Add test and passing react code to Input

--- src/Input.js
+++ src/Input.js
@@ -3,12 +3,29 @@ import { connect } from "react-redux";
 
 class Input extends Component {
   render() {
-    return <div>yeah</div>;
+    const content = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guessed word"
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    );
+    return <div data-test="component-input">{content}</div>;
   }
 }
 
-const mapStateToProps = state => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
--- src/test/Input.test.js
+++ src/test/Input.test.js
@@ -4,6 +4,12 @@ import { shallow } from "enzyme";
 import { findByTestAttr, storeFactory } from "./testUtils";
 import Input from "../Input";
 
+/**
+ * Returns the child component of the connected component hooked up to the test store.
+ * @param {object} initialState
+ * @function setup
+ * @returns wrapper - The shallow rendered child component we got after diving
+ */
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
   const wrapper = shallow(<Input store={store} />)
@@ -12,7 +18,7 @@ const setup = (initialState = {}) => {
 
   return wrapper;
   // console.log(wrapper.debug());
-  // This gave us the HigherOrder Component that redux wraps the shallow rendered component in.
+  // This gave us the HigherOrder Component that redux via connect wraps the shallow rendered component in.
   // <ContextProvider value={{...}}>
   //   <Input store={{...}} dispatch={[Function: dispatch]} />
   // </ContextProvider>
@@ -20,11 +26,26 @@ const setup = (initialState = {}) => {
 
 describe("render", () => {
   describe("while the word has not been guessed", () => {
-    test("it renders the component without error", () => {});
-    test("it renders input box", () => {});
-    test("it renders submit box", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+
+    test("it renders the component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("it renders input box", () => {
+      const component = findByTestAttr(wrapper, "input-box");
+      expect(component.length).toBe(1);
+    });
+    test("it renders submit box", () => {
+      const component = findByTestAttr(wrapper, "submit-button");
+      expect(component.length).toBe(1);
+    });
   });
-  describe("word has been guessed", () => {
+  describe("when word has been guessed", () => {
     test("it renders component without error", () => {});
     test("it does not render input box", () => {});
     test("it does not render submit button", () => {});
